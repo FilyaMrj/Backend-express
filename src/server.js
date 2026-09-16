02620984d02ab39aaf9f5a87f792a514340ade78
FilyaMrj <filyamaharjan98@gmail.com>
2026-09-16 08:51:27 +0545

    added

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const expresss = require ("express");
+const app = expresss()
+const port = 3000
+
+app.get('/',(req, res)=> {
+    res.send('hello world!')
+})
+
+app.listen(port, () =>{
+    console.log(`Example app listening on port ${port}`)
+})
